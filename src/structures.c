0f734ecc6c05bb0376bb663884a3335b5ac9df48
René Perschon <chummer_rene@gmx.net>
2011-10-28 12:29:08 +0200

    improved OO model

--- src/structures.c
+++ src/structures.c
@@ -34,7 +34,6 @@ struct Element_data
 {
   uint8_t  type_id;
   struct Element* parent;
-  char*    text;
 };
 
 struct Element
@@ -51,13 +50,13 @@ struct Node_data
 {
   struct Element* yes;
   struct Element* no;
+  char*           question;
 };
 
 struct Node
 {
-  struct Element_op*   ops;
-  struct Element_data* data;
-  struct Node_data*    ndata;
+  struct Element*      super;
+  struct Node_data*    data;
 };
 
 /******************************
@@ -66,14 +65,13 @@ struct Node
 
 struct Leaf_data
 {
-
+  char* name;
 };
 
 struct Leaf
 {
-  struct Element_op*   ops;
-  struct Element_data* data;
-  struct Leaf_data*    ldata;
+  struct Element*   super;
+  struct Leaf_data* data;
 };
 
 void
@@ -87,8 +85,13 @@ struct Node*
 new_node()
 {
   struct Node* this = malloc(sizeof(struct Node));
+
   init_element((struct Element*)this);
-  this->data->type_id = TYPE_NODE;
+  this->super->data->type_id = TYPE_NODE;
+
+  this->data->question = NULL;
+  this->data->yes = NULL;
+  this->data->no = NULL;
 
   return this;
 }
@@ -97,8 +100,11 @@ struct Leaf*
 new_leaf()
 {
   struct Leaf* this = malloc(sizeof(struct Leaf));
+
   init_element((struct Element*)this);
-  this->data->type_id = TYPE_LEAF;
+  this->super->data->type_id = TYPE_LEAF;
+
+  this->data->name = NULL;
 
   return this;
 }
