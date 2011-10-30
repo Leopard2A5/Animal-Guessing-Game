468178be0a5927b0fa8556b30f7b10a536b542e0
carsten <carsten@XPS>
2011-10-30 16:40:49 +0100

    erster Programmablauf eingepflegt argumentaufruf integriert

--- src/Animal-Guessing-Game.c
+++ src/Animal-Guessing-Game.c
@@ -13,8 +13,44 @@
 #include "structures.h"
 
 int
-main(void)
+main(int argc, char* argv[])
 {
-  puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+  puts("Animal Guessing Game"); /* prints Animal Guessing Game */
+  FILE *datei;
+  printf("Anzahl Argumente %d.\n",argc);
+  if (argc > 1)         //es wurde ein argument uebergeben
+  {
+    datei = fopen (argv[1], "r+");
+    if (datei == NULL) //falls dieser dateinamen nicht existiert
+      {
+        printf("Fehler beim Oeffnen der Datei.\n");
+        char answer='0';
+        while (!((answer == 'y') | (answer == 'n')))
+        {
+          printf("Neue Datei anlegen [y/n]:");
+          answer = getchar(); /*nicht die richtige art einzulesen
+                              schleife wird mehrfach durchlaufen s
+                              obald mehrere zeichen eingegeben*/
+          if (answer == 'y')
+          {
+            //dateinamen fragen und argument als vorschlag nutzen
+            printf("Bitte Dateinamen angeben [%s]:\n",argv[1]);
+            //neuen namen einlesen und dateistream oeffnen
+          }
+        }
+      }
+    else
+    {
+      //versuche dateiinhalt lesen und baum bauen
+    }
+  }
+
+  //spiel spielen
+  //speicheroption integrieren
+
+  //Dateistream schliessen
+  if (datei != NULL)
+  fclose (datei);
+  printf("Programmende.\n");
   return EXIT_SUCCESS;
 }
