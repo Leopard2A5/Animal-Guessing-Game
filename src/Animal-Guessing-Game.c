03ac53efeac80939c4cd3978f7813be883b8d39b
carsten <carsten@XPS>
2011-10-31 17:09:55 +0100

    restructured game-menu ability to enter one char first attempt of element usage clear-screen method integrated

--- src/Animal-Guessing-Game.c
+++ src/Animal-Guessing-Game.c
@@ -8,50 +8,75 @@
  ============================================================================
  */
 
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "structures.h"
 #include "util.h"
 
+Element* root;
+
+void
+cls(void)
+{
+  printf("\033[2J"); //terminalsequenz zum loeschen des inhalts
+}
+
+char
+read_single_Char(void)
+{
+  char input[100];
+  scanf("%99s", &input[0]);
+  input[0]=toupper(input[0]); // um gross/kleinschreibung zu eliminieren
+  return input[0];
+}
+
+void
+start_game(void)
+{
+  printf("Please enter an animal:\n");
+  char animal[100];
+  scanf("%99s", &animal[0]);
+}
+
+void
+game_menu(void)
+{
+  char input;
+  do
+  {
+    printf("Animal Guessing Game - Menu\n\n");
+    printf("[1] Start a new Game\n");
+    printf("[2] Save\n");
+    printf("[3] Abort\n");
+    input=read_single_Char();
+    switch(input)
+    {
+      case '1':
+        start_game();
+        break;
+      case '2':
+        printf("Save\n");
+        break;
+      case '3':
+        printf("Quitting program!\n");
+        exit(0);
+        break;
+      default:
+        printf("Wrong Input!\n"); ;
+        break;
+    }
+  } while (input != 3);
+}
+
 int
 main(int argc, char* argv[])
 {
-  puts("Animal Guessing Game"); /* prints Animal Guessing Game */
-  FILE *datei;
-  printf("Anzahl Argumente %d.\n",argc);
-  if (argc > 1)         //es wurde ein argument uebergeben
-  {
-    datei = fopen (argv[1], "r+");
-    if (datei == NULL) //falls dieser dateinamen nicht existiert
-      {
-        printf("Fehler beim Oeffnen der Datei.\n");
-        char answer='0';
-        while (!((answer == 'y') | (answer == 'n')))
-        {
-          printf("Neue Datei anlegen [y/n]:");
-          answer = getchar(); /*nicht die richtige art einzulesen
-                              schleife wird mehrfach durchlaufen s
-                              obald mehrere zeichen eingegeben*/
-          if (answer == 'y')
-          {
-            //dateinamen fragen und argument als vorschlag nutzen
-            printf("Bitte Dateinamen angeben [%s]:\n",argv[1]);
-            //neuen namen einlesen und dateistream oeffnen
-          }
-        }
-      }
-    else
+  cls();
+  if(argc>1)
     {
-      //versuche dateiinhalt lesen und baum bauen
+     root = load_tree(argv[1]);
     }
-  }
-
-  //spiel spielen
-  //speicheroption integrieren
-
-  //Dateistream schliessen
-  if (datei != NULL)
-  fclose (datei);
-  printf("Programmende.\n");
+  game_menu();
   return EXIT_SUCCESS;
 }
